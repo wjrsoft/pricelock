db59bba880f74638059d77f8481073b1065a3612
wjrsoft <wjrsoftware@163.com>
2020-05-20 23:09:51 +0800

    新建一个项目

---
+++ priceclock-web/src/main/java/com/jin/controller/GetCNYPrice.java
@@ -0,0 +1,52 @@
+package com.jin.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.jin.utils.HttpClientUtil;
+
+/**
+ * 获取外汇价格
+ */
+@Controller
+public class GetCNYPrice {
+	private final static Logger logger = LoggerFactory.getLogger(GetCNYPrice.class);
+	
+	
+	@SuppressWarnings({ "unused", "static-access" })
+	@RequestMapping("/getCNYPrice.do")
+	@ResponseBody
+	public String getCNYPrice(HttpServletRequest req, HttpServletResponse resp) {
+		//允许跨域请求
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+		String json = "";
+		System.out.println("进入getCNYPrice.do");
+		try {
+			JSONObject jsonObject = HttpClientUtil.doGet(
+					"http://api.k780.com/?app=finance.shgold&goldid=1052&version=3&appkey=51412&sign=99a291c772ca77fd6aee3906f0fadc4e&format=json");
+			json=jsonObject.toJSONString();
+			logger.info("价格[{}]",json);
+		} catch (Exception e) {
+			logger.error("异常[{}]",e);
+		}
+		return json;
+	}
+	
+	
+	public String getPrice(String json) {
+		JSONObject jsonObject = JSONObject.parseObject(json);
+		Object obj = jsonObject.get("records");
+		logger.info(JSON.toJSONString(obj));
+		return JSON.toJSONString(obj);
+	}
+
+	
+}
---
+++ priceclock-web/src/main/java/com/jin/utils/HttpClientUtil.java
@@ -0,0 +1,84 @@
+package com.jin.utils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import com.alibaba.fastjson.JSONObject;
+/**
+ * doGet和doPost请求
+ * @author jinrong.wang
+ *
+ */
+public class HttpClientUtil {
+	/**
+	 * 未经过URI转码
+	 * get请求,暂时项目中用到个方法主要是用于获取token
+	 * @param url
+	 * @return
+	 * @throws ParseException
+	 * @throws IOException
+	 */
+	public static JSONObject doGetStr(String url) throws ParseException, IOException{
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(url);
+		JSONObject jsonObject = null;
+		HttpResponse httpResponse = client.execute(httpGet);
+		HttpEntity entity = httpResponse.getEntity();
+		if(entity != null){
+			String result = EntityUtils.toString(entity,"UTF-8");
+			jsonObject = JSONObject.parseObject(result);
+		}
+		return jsonObject;
+	}
+	
+	/**
+	 * URI转码的
+	 * @param url
+	 * @return
+	 * @throws Exception
+	 */
+	public static JSONObject doGet(String url) throws Exception {
+		// 创建一个httpclient链接对象
+		HttpClient httpclient = new DefaultHttpClient();
+		URL mURL = new URL(url);
+		URI uri = new URI(mURL.getProtocol(), mURL.getHost(), mURL.getPath(), mURL.getQuery(), null);
+		HttpGet httpGet = new HttpGet(uri);
+		ResponseHandler<String> responseHandler = new BasicResponseHandler();
+		// 执行请求并获取结果
+		String responseBody = httpclient.execute(httpGet, responseHandler);
+		JSONObject jsonObject = JSONObject.parseObject(responseBody);
+		return jsonObject;
+	}
+	
+	/**
+	 * POST请求,目前用于这个的有创建菜单
+	 * @param url
+	 * @param outStr
+	 * @return JSONObject
+	 * @throws ParseException
+	 * @throws IOException
+	 */
+	public static JSONObject doPostStr(String url,String outStr) throws ParseException, IOException{
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpPost httpost = new HttpPost(url);
+		JSONObject jsonObject = null;
+		httpost.setEntity(new StringEntity(outStr,"UTF-8"));
+		HttpResponse response = client.execute(httpost);
+		String result = EntityUtils.toString(response.getEntity(),"UTF-8");
+		jsonObject = JSONObject.parseObject(result);
+		return jsonObject;
+	}
+}
